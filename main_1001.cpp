f832169b84ae3ab9412cc9a6e1204e1bcdb98610
misha <a2nced.c0mputin9.lms.17551@yandex.ru>
2020-02-15 14:48:31 +0300

    1001 is done

--- main_1001.cpp
+++ main_1001.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
-
+#include <cmath>
+#include <iomanip>
+#include <list>
 
 int main(int argc, const char * argv []) {
 
-    std::cout << "--> " << std::endl;
+    double a;
+    std::list<double> buffer;
+    while (std::cin >> a) {
+        buffer.push_back(a);
+    }
 
-
+    for (std::list<double>::const_reverse_iterator itr = buffer.crbegin(); itr != buffer.crend(); ++itr) {
+        std::cout << std::fixed << std::setprecision(4) << std::sqrt(*itr) << std::endl;
+    }
 
     return 0;
 }
