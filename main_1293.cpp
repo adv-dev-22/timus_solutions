c4352338bf2f009d1d0400a68857c72fb994a491
misha <a2nced.c0mputin9.lms.17551@yandex.ru>
2020-02-15 16:47:25 +0300

    1293 is done

---
+++ main_1293.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char * argv []) {
+
+    size_t N, A, B;
+    std::cin >> N >> A >> B;
+
+    std::cout << (2* N * A * B) << std::endl;
+
+    return 0;
+}
