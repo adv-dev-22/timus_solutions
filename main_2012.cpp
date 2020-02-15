4049e71705931bcc3035aa1072bb20b373dde31c
misha <a2nced.c0mputin9.lms.17551@yandex.ru>
2020-02-15 16:56:40 +0300

    2012 passed

---
+++ main_2012.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char * argv []) {
+
+    size_t f;
+    std::cin >> f;
+
+    const size_t N = 12;
+    if ((12 - f) * 45 <= 240) std::cout << "YES" << std::endl;
+    else std::cout << "NO" << std::endl;
+
+    return 0;
+}
