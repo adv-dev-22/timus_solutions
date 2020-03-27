ce89d5d7e6041322302b6d8ac02ba0c850e28f68
misha <a2nced.c0mputin9.lms.17551@yandex.ru>
2020-03-27 11:25:11 +0300

    fixed wrong test 14

--- task_1787.cpp
+++ task_1787.cpp
@@ -6,21 +6,17 @@ int main(int argc, char * argv []) {
     int n_minutes;
 
     std::cin >> k_turn >> n_minutes;
-
-    int sum = 0;
+    int rest = 0;
     for (int i = 0; i < n_minutes; ++i) {
         int a_i = 0;
     	std::cin >> a_i;
-        sum += a_i;
-    }
 
-    int rest = sum - k_turn * n_minutes;
-    if (rest < 0) rest = 0;
+        rest += a_i;
+        if (rest - k_turn < 0) rest = 0;
+        else rest -= k_turn;
+    }
 
     std::cout << rest << std::endl;
 
     return 0;
 }
-
-
-
