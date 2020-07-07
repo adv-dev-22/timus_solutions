4b83a3fee03ace3fcd0b9062bc2beab8561c70f2
adv-dev-22 <a2nced.c0mputin9.lms.17551@yandex.ru>
2020-07-07 15:26:32 +0300

    history lesson

--- task_1196.cpp
+++ task_1196.cpp
@@ -1,4 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
+void encrease_position_if_exists(const int year_stud,
+                                 std::vector<int> & ref_dates,
+                                 int & counter) {
 
+    auto found_item = std::lower_bound(ref_dates.begin(), ref_dates.end(), year_stud);
+
+    if (found_item != ref_dates.end() && *found_item == year_stud) {
+        counter++;
+    }
+}
+
+int main(int argc, char * argv []) {
+
+    int N{0};
+    std::cin >> N;
+    std::vector<int> ref_dates;
+    ref_dates.reserve(N);
+
+    // Add first element
+    int year_value;
+    std::cin >> year_value;
+    ref_dates.emplace_back(year_value);
+
+    // Second and more
+    for (int i = 1; i < N; ++i) {
+        scanf("%d", &year_value);
+        if (year_value > ref_dates.back()) {
+            ref_dates.emplace_back(year_value);
+        }
+    }
+
+    int M{0};
+    std::cin >> M;
+
+    int counter{0};
+
+    for (int k = 0; k < M; ++k) {
+        int year_stud{0};
+        scanf("%d", &year_stud);
+        encrease_position_if_exists(year_stud, ref_dates, counter);
+    }
+    std::cout << counter << std::endl;
+
+    return 0;
+}
 
 // End of the file
